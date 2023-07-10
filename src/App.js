1115e8e884ed16aa81684fc261f92aefb72188a1
Ganesh <gspjnv@gmail.com>
2023-07-10 10:22:17 +0530

    smartwatch buying application

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import ProductData from './components/Productdata';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="App">
+       <ProductData/>  
+
+
+
+      </div>
+    </>
+  );
+}
+
+export default App;
